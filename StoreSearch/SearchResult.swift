ff2973b3caafded2f8e17b5354fc717a0e2f8c0f
tsinghqs@vt.edu <tsinghqs@vt.edu>
2018-06-27 14:26:05 -0400

    Search Bar functionality

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Tarun Singh on 6/27/18.
+//  Copyright © 2018 Tarun Singh. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class SearchViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
-    
     @IBOutlet var searchBar: UISearchBar!
+    
+    var searchResults: [String] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,13 +25,47 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    extension SearchViewController: UITableViewDelegate {
+
+
+    
+    }
+extension SearchViewController: UITableViewDataSource{
+    //to be implemented
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    func tableView (_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+        
+        cell.textLabel!.text = searchResults[indexPath.row]
+        return cell
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
         //to be implemented
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searchResults = []
+        
+        for i in 0...2 {
+            searchResults.append(String(format: "Fake Result %d for '%@'", i, searchBar.text!))
+        }
+        
+        tableView.reloadData()
         
     }
-    extension SearchViewController: UITableViewDataSource{
-        //to be implemented
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
     }
-
 }
+
 
